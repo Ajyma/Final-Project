72b03095a7f70c91c08d7511b26c054be7f534b0
IndiraErgashova <indiraergashova@gmail.com>
2022-06-03 04:36:28 +0600

    i added slider and cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { Layout } from './pages/Layout';
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <Layout/>
+      
     </React.Fragment>
   );
 }
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { SliderList } from "../../utils"; 
+import './Slider.scss'
+import { Btn } from "./Button";
+
+export const Slider = () => {
+
+    const [sliderIndex , setSliderIndex] = React.useState(1)
+
+    const prevSlide = () => {
+        if(sliderIndex !== 1){
+            setSliderIndex(sliderIndex - 1)
+        }else if(sliderIndex === 1){
+            setSliderIndex(SliderList.length)
+        }
+    }
+
+    const nextSlide = () => {
+        if(sliderIndex !== SliderList.length){
+            setSliderIndex(sliderIndex + 1)
+        }else if(sliderIndex === SliderList.length){
+            setSliderIndex(1)
+        }
+    }
+    return (
+        <>
+            <div className='sliderParent'>
+                {
+                    SliderList.map((img , index) => <img src={img.url} alt='none' key={img.id} className={sliderIndex === index + 1 ?'image active' : 'image' }/>)
+                }
+                <Btn direction='prev' handleSlide={prevSlide}/>
+                <Btn direction='next' handleSlide={nextSlide}/>
+            </div>
+
+        </>
+    )
+}
