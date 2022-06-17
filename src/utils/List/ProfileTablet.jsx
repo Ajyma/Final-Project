30c22886aad4dc7e4f07d29cc1a6652110cc3779
Ajyma <ajymakockorova@gmail.com>
2022-06-17 16:38:53 +0600

    added

--- src/utils/List/ProfileTablet.jsx
+++ src/utils/List/ProfileTablet.jsx
@@ -8,7 +8,7 @@ const ProfileTablet = ({isToggle, toggle}) => {
 
   return (
     <div className={isToggle ? 'tabletProfile active' : 'tabletProfile'}>
-      <img src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="" />
+      <img src={users.photo} alt="" />
       <p>{users.name}</p>
       <hr />
       <h4 onClick={handleSignOut}>
